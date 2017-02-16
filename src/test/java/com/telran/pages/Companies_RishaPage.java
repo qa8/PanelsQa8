16b76ad72636c705feb55b6ca5f15267f2bdd981
Risha7 <vodyanaya.ia@gmail.com>
2017-02-16 23:09:50 +0200

    Added the new methods

--- src/test/java/com/telran/pages/Companies_RishaPage.java
+++ src/test/java/com/telran/pages/Companies_RishaPage.java
@@ -10,7 +10,7 @@ import org.openqa.selenium.support.PageFactory;
 /**
  * Created by Boss on 11.02.2017.
  */
-public class Companies_RishaPage extends LoginIakovPage {
+public class Companies_RishaPage extends Page {
     @FindBy(how = How.TAG_NAME, using = "h1")
 
     @CacheLookup
