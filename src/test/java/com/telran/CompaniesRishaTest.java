24efde4eabe2e74cde4b690838f4bbaacb9cab85
Risha7 <vodyanaya.ia@gmail.com>
2017-02-18 21:51:12 +0200

    Companies test is added

---
+++ src/test/java/com/telran/CompaniesRishaTest.java
@@ -0,0 +1,37 @@
+package com.telran;
+
+//import com.telran.pages.HomePage;
+
+import com.telran.pages.CompaniesPage_Risha;
+import com.telran.pages.LoginRishaPage;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+
+public class CompaniesRishaTest extends TestNgTestBase {
+    public LoginRishaPage loginRishaPage;
+    public CompaniesPage_Risha companiesPage_Risha;
+    private String url = "https://greengnome.github.io/panels/#/companies";
+
+    @BeforeMethod
+    public void initPageObjects() {
+
+        loginRishaPage = PageFactory.initElements(driver, LoginRishaPage.class);
+        companiesPage_Risha = PageFactory.initElements(driver, CompaniesPage_Risha.class);
+    }
+
+    public void positiveMenuCompaniesTest() {
+        driver.get(url);
+        companiesPage_Risha.waitForCompaniesPageInLoaded();
+        companiesPage_Risha.clickOnManagementButton();
+        // companiesPage_Risha.selectValueInDropdownManagement();
+        companiesPage_Risha.clickOnSurveysButton();
+        //companiesPage_Risha.selectValueInDropdownSurveys();
+        companiesPage_Risha.clickOnReportsButton();
+        // companiesPage_Risha.selectValueInDropdownReports();
+        companiesPage_Risha.clickOnAlertsButton();
+        companiesPage_Risha.clickOnQuitButton();
+        loginRishaPage.waitForLoginPageIsLoaded();
+        Assert.assertTrue(loginRishaPage.isOnLoginPage());
+    }
+}
