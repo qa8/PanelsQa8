686ec09892b3888f46a7ea765cd8a7f9f27750bb
stas9141 <dbrmlsky@gmail.com>
2017-02-19 15:51:50 +0200

    add methods

--- src/test/java/com/telran/pages/CompaniesStasPage.java
+++ src/test/java/com/telran/pages/CompaniesStasPage.java
@@ -1,7 +1,19 @@
 package com.telran.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 /**
  * Created by user on 18.02.2017.
  */
 public class CompaniesStasPage {
+    //tadiranProjectButton
+    @FindBy(xpath = "//*[class='mdl-grid']/div[1]//a")
+    public WebElement tadiranProjectButton;
+    //diklaProjectButton
+    @FindBy(xpath = "//*[class='mdl-grid']/div[2]//a")
+    public WebElement clalitProjectButton;
+    //clalitProjectButton
+    @FindBy(xpath = "//*[class='mdl-grid']/div[3]//a")
+    public WebElement diklaProjectButton;
 }
