ec2821f5dee9464d01cb850665ccc62aeedabac6
Risha7 <vodyanaya.ia@gmail.com>
2017-02-21 23:23:25 +0200

    added Companies Page

--- src/test/java/com/telran/CompaniesRishaTest.java
+++ src/test/java/com/telran/CompaniesRishaTest.java
@@ -12,7 +12,7 @@ import org.testng.annotations.Test;
 public class CompaniesRishaTest extends TestNgTestBase {
     public LoginRishaPage loginRishaPage;
     public CompaniesPage_Risha companiesPage_Risha;
-    private String url = "https://greengnome.github.io/panels/#/companies";
+    private String url = "https://greengnome.github.io/panels/#/login";
 
     @BeforeMethod
     public void initPageObjects() {
@@ -20,17 +20,22 @@ public class CompaniesRishaTest extends TestNgTestBase {
         loginRishaPage = PageFactory.initElements(driver, LoginRishaPage.class);
         companiesPage_Risha = PageFactory.initElements(driver, CompaniesPage_Risha.class);
     }
-
     @Test
     public void positiveMenuCompaniesTest() {
         driver.get(url);
+        loginRishaPage.waitForLoginPageIsLoaded();
+        loginRishaPage.fillLoginField("admin");
+        loginRishaPage.fillPasswordField("12345");
+        loginRishaPage.clickOnLogButtonField();
         companiesPage_Risha.waitForCompaniesPageInLoaded();
-        companiesPage_Risha.clickOnManagementButton();
-        companiesPage_Risha.clickOnSurveysButton();
+        // companiesPage_Risha.clickOnManagementButton();
+        // companiesPage_Risha.selectValueInDropdownManagement();
+        // companiesPage_Risha.clickOnSurveysButton();
         //companiesPage_Risha.selectValueInDropdownSurveys();
-        companiesPage_Risha.clickOnReportsButton();
+        //companiesPage_Risha.clickOnReportsButton();
         // companiesPage_Risha.selectValueInDropdownReports();
-        companiesPage_Risha.clickOnAlertsButton();
+        //companiesPage_Risha.clickOnAlertsButton();
+        // companiesPage_Risha.selectValueInDropdownAlerts();
         companiesPage_Risha.clickOnQuitButton();
         loginRishaPage.waitForLoginPageIsLoaded();
         Assert.assertTrue(loginRishaPage.isOnLoginPage());
