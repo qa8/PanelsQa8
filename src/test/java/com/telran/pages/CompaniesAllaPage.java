35e79c8fba15d570776f392cb18f81f95223e73f
yacov <jakoff@gmail.com>
2017-02-22 18:25:34 +0200

    Changed class

--- src/test/java/com/telran/pages/CompaniesAllaPage.java
+++ src/test/java/com/telran/pages/CompaniesAllaPage.java
@@ -1,9 +1,9 @@
 package com.telran.pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.PageFactory;
 
 
@@ -11,21 +11,25 @@ import org.openqa.selenium.support.PageFactory;
  * Created on 10.02.2017.
  */
 public class CompaniesAllaPage extends Page {
-    @FindBy(how = How.TAG_NAME, using = "h1")
-
-    public WebElement header;
 
 
-    //@FindBy(xpath = "//*[@class='adminMenu']")
-    //public WebElement adminMenuBlock;
+    //ElementsBlock for wait method
+    @FindBy(xpath = "//*[@class='adminMenu']")
+    public WebElement adminMenuBlock;
 
-    @FindBy(xpath = "//h2[contains(text(),'Tadiran')]/../..//a")
+    //tadiranProjectButton
+    @FindBy(xpath = "//*[contains(@style,'tadiran')]/..//a")
+    public WebElement tadiranbyNameProjectButton;
+
+    @FindBy(xpath = "//*[@class='mdl-grid']/div[1]//a")
     public WebElement tadiranProjectButton;
 
-
-    //@FindBy(xpath = "//*[@class='mdl-grid']/div[1]//a")
-    //public WebElement tadiranProjectButton;
-
+    //diklaProjectButton
+    @FindBy(xpath = "//*[@class='mdl-grid']/div[2]//a")
+    public WebElement clalitProjectButton;
+    //clalitProjectButton
+    @FindBy(xpath = "//*[@class='mdl-grid']/div[3]//a")
+    public WebElement diklaProjectButton;
 
     @FindBy(xpath = "//select[@ng-model='selectedLanguage']")
     public WebElement selectLanguage;
@@ -36,14 +40,13 @@ public class CompaniesAllaPage extends Page {
     @FindBy(xpath = "//select[@ng-model='selectedLanguage']/option[2]")
     public WebElement selectHebrewLanguage;
 
-
     public CompaniesAllaPage(WebDriver driver) {
         super(driver);
         PageFactory.initElements(driver, this);
     }
     //methods
     public void waitForCompaniesPageInLoaded() {
-        waitUntilIsLoadedCustomTime(tadiranProjectButton, 40);
+        waitUntilIsLoadedCustomTime(adminMenuBlock, 40);
     }
 
     public boolean isOnCompaniesPage() {
@@ -54,17 +57,31 @@ public class CompaniesAllaPage extends Page {
         clickElement(tadiranProjectButton);
     }
 
-    // public WebElement findCompanyProjectButton(String company) {
-    // return driver.findElement(By.xpath("//h2[Contains(Text(),"+ company +")]/../..//a"));}
+    public void clickProjectButtonbyName(String company) {
+        String s = "//h2[contains(Text(),'" + company + "')]/../..//a";
+        driver.findElement(By.xpath(s)).click();
+    }
 
+    public void clickClalitProjectButton() {
+        clickElement(clalitProjectButton);
+    }
+
+    public void clickDiclaProjectButton() {
+        clickElement(diklaProjectButton);
+    }
 
     public void selectEnglishLanguage() {
         selectValueInDropdownbyText(selectLanguage, "English");
     }
 
+    public void selectEnglishLanguagebyValue() {
+        selectValueInDropdown(selectLanguage, "En");
+    }
+
     public boolean englishLanguageIsSelected() {
         return exists(selectEnglishLanguage);
     }
+
     public void selecthebrewLanguage() {
         selectValueInDropdownbyText(selectLanguage, "עברית");
     }
