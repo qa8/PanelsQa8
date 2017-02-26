8e6626d4d74bac8718718dbcdaf8788b989cfdf9
MarinaA2015 <marina.abrashkina2015@gmail.com>
2017-02-26 14:35:06 +0200

    some cosmetic changes

--- src/test/java/com/telran/LoginMarinaTest.java
+++ src/test/java/com/telran/LoginMarinaTest.java
@@ -1,7 +1,9 @@
 package com.telran;
 
 import com.telran.pages.CompaniesMarinaPage;
+import com.telran.pages.HeaderMarinaPage;
 import com.telran.pages.LoginMarinaPage;
+import com.telran.pages.NavigationMarinaPage;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
@@ -12,35 +14,40 @@ public class LoginMarinaTest extends TestNgTestBase {
     private static final String URL_LINK = "https://greengnome.github.io/panels/?#/login";
     private static final String PSW_ADM = "12345";
     private static final String LOGIN_ADM ="Admin" ;
+    private static final String NAME_COMPANY ="Tadiran" ;
 
 
     private LoginMarinaPage loginMarinaPage;
     private CompaniesMarinaPage companiesMarinaPage;
+    private NavigationMarinaPage navigationMarinaPage;
+    private HeaderMarinaPage headerMarinaPage;
 
     @BeforeMethod
     public void initPageObjects() {
         loginMarinaPage = PageFactory.initElements(driver, LoginMarinaPage.class);
         companiesMarinaPage = PageFactory.initElements(driver,CompaniesMarinaPage.class);
+        navigationMarinaPage = PageFactory.initElements(driver, NavigationMarinaPage.class);
+        headerMarinaPage = PageFactory.initElements(driver, HeaderMarinaPage.class);
+        driver.get(URL_LINK);
+        loginMarinaPage.waitForLoginPageIsLoadedTime50();
 }
 
     @Test
     public void negativeLoginTest() {
-        driver.get(URL_LINK);
-        loginMarinaPage.waitForLoginPageIsLoadedTime50();
-        loginMarinaPage.fillLoginField("sss");
-        loginMarinaPage.fillPassField("pppp");
-        loginMarinaPage.clickOnLoginButton();
-        loginMarinaPage.waitForLoginPageIsLoaded();
+
+        loginMarinaPage.fillLoginField("sss")
+                        .fillPassField("pppp")
+                        .clickOnLoginButton()
+                        .waitForLoginPageIsLoaded();
         Assert.assertTrue(loginMarinaPage.isOnLoginPage());
     }
 
     @Test
     public void negativeLoginTestPswIncorrect() {
-        driver.get(URL_LINK);
-        loginMarinaPage.waitForLoginPageIsLoadedTime50();
-        loginMarinaPage.fillLoginField(LOGIN_ADM);
-        loginMarinaPage.fillPassField("hjkwi");
-        loginMarinaPage.clickOnLoginButton();
+
+        loginMarinaPage.fillLoginField(LOGIN_ADM)
+                        .fillPassField("hjkwi")
+                        .clickOnLoginButton();
         //companiesMarinaPage.waitForCompanyLoaded("Tadiran"); - it doesn't work (with dynamic element)
         companiesMarinaPage.waitForTadiranProjectLoaded();
         Assert.assertTrue(loginMarinaPage.isOnLoginPage());
@@ -48,14 +55,17 @@ public class LoginMarinaTest extends TestNgTestBase {
 
 
     @Test
-    public void positiveAdminLoginTest() {
-        driver.get(URL_LINK);
-        loginMarinaPage.waitForLoginPageIsLoadedTime50();
-        loginMarinaPage.fillLoginField(LOGIN_ADM);
-        loginMarinaPage.fillPassField(PSW_ADM);
-        loginMarinaPage.clickOnLoginButton();
-        //companiesMarinaPage.waitForCompanyLoaded("Tadiran"); - it doesn't work (with dynamic element)
+    public void positiveAdminLoginTest() throws InterruptedException {
+
+        loginMarinaPage.fillLoginField(LOGIN_ADM)
+                        .fillPassField(PSW_ADM)
+                        .clickOnLoginButton();
+        //companiesMarinaPage.waitForCompanyLoaded(NAME_COMPANY);// - it doesn't work (with dynamic element)
+        navigationMarinaPage.waitManagementListIsLoadedTime50();
+        headerMarinaPage.waitHeaderIsLoaded();
         companiesMarinaPage.waitForTadiranProjectLoaded();
+        companiesMarinaPage.isCompanyOnCompaniesPage("Tadiran");
+
         Assert.assertTrue(companiesMarinaPage.isCompanyOnCompaniesPage("Tadiran"));
 
 
