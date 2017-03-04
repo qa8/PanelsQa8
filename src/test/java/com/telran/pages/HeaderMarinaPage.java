320b90193d0079e4883e8d354a61fd84a533ee9c
MarinaA2015 <marina.abrashkina2015@gmail.com>
2017-03-04 09:54:43 +0200

    CompaniesMarinaPage: logging was added and switching language

--- src/test/java/com/telran/pages/HeaderMarinaPage.java
+++ src/test/java/com/telran/pages/HeaderMarinaPage.java
@@ -1,5 +1,7 @@
 package com.telran.pages;
 
+import com.telran.LogLog4j;
+import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -7,6 +9,7 @@ import org.openqa.selenium.support.PageFactory;
 
 
 public class HeaderMarinaPage extends Page{
+    private static Logger Log = Logger.getLogger(LogLog4j.class.getName());
     public HeaderMarinaPage(WebDriver driver) {
         super(driver);
         PageFactory.initElements(driver, this);
@@ -28,16 +31,20 @@ public class HeaderMarinaPage extends Page{
 
     //methods
     public void chooseHebrewLanguage(){
+        Log.info("Choosing hebrew language ");
         selectValueInDropdown(languageSelector,"he");
     }
     public void chooseEnglishLanguage(){
+        Log.info("Choosing english language ");
         selectValueInDropdown(languageSelector,"en");
     }
 
     public void returnToCompanies(){
+        Log.info("Go to home ");
         goToHome.click();
     }
     public void waitHeaderIsLoaded(){
+        Log.info("Waiting  until header page is loaded ");
         waitUntilIsLoaded(goToHome);
     }
 
