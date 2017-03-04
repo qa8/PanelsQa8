fe0b834d3046901853a58ec4e3d16990534d50c8
MarinaA2015 <marina.abrashkina2015@gmail.com>
2017-03-04 09:55:24 +0200

    LoginMarinaPage: logging was added

--- src/test/java/com/telran/pages/LoginMarinaPage.java
+++ src/test/java/com/telran/pages/LoginMarinaPage.java
@@ -1,5 +1,7 @@
 package com.telran.pages;
 
+import com.panel.LogLog4j;
+import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -10,6 +12,7 @@ import org.openqa.selenium.support.PageFactory;
  * Sample page
  */
 public class LoginMarinaPage extends Page {
+    private static Logger Log = Logger.getLogger(LogLog4j.class.getName());
 
     @FindBy(how = How.TAG_NAME, using = "h1")
     public WebElement header;
@@ -33,29 +36,35 @@ public class LoginMarinaPage extends Page {
 
     //methods
     public LoginMarinaPage fillLoginField(String text) {
+        Log.info("Filling login field: "+text);
         this.setElementText(loginField, text);
         return this;
     }
 
     public LoginMarinaPage fillPassField(String text) {
+        Log.info("Filling password field: "+text);
         this.setElementText(passwordField, text);
         return this;
     }
 
     public LoginMarinaPage clickOnLoginButton() {
+        Log.info("Clicking to login button ");
         this.clickElement(loginButton);
         return this;
     }
     public LoginMarinaPage waitForLoginPageIsLoaded() {
+        Log.info("Waiting for login page");
         waitUntilIsLoaded(loginButton);
         return this;
     }
     public LoginMarinaPage waitForLoginPageIsLoadedTime50() {
+        Log.info("Waiting for login page (time 50)");
         waitUntilIsLoadedCustomTime(loginButton, 50);
         return this;
     }
 
     public boolean isOnLoginPage(){
-    return exists(pleaseLogInHeader);
+        Log.info("Verification: Is on login page (login header exists)");
+        return exists(pleaseLogInHeader);
     }
 }
