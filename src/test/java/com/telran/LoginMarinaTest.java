95c0bb382b9c7cc90de54ffb6834cfd5be72d5f4
MarinaA2015 <marina.abrashkina2015@gmail.com>
2017-03-26 12:01:12 +0300

    (alwaysRun = true) were added for @BeforeSuite, @BeforeClass, @AfterSuite

--- src/test/java/com/telran/LoginMarinaTest.java
+++ src/test/java/com/telran/LoginMarinaTest.java
@@ -4,12 +4,16 @@ import com.telran.pages.CompaniesMarinaPage;
 import com.telran.pages.HeaderMarinaPage;
 import com.telran.pages.LoginMarinaPage;
 import com.telran.pages.NavigationMarinaPage;
+import org.apache.log4j.Logger;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import ru.stqa.selenium.factory.WebDriverPool;
 
 public class LoginMarinaTest extends TestNgTestBase {
+    private static Logger Log = Logger.getLogger(LogLog4j.class.getName());
 
     private static final String URL_LINK = "https://greengnome.github.io/panels/?#/login";
     private static final String PSW_ADM = "12345";
@@ -22,7 +26,8 @@ public class LoginMarinaTest extends TestNgTestBase {
     private NavigationMarinaPage navigationMarinaPage;
     private HeaderMarinaPage headerMarinaPage;
 
-    @BeforeMethod
+
+    @BeforeMethod(alwaysRun = true)
     public void initPageObjects() {
         loginMarinaPage = PageFactory.initElements(driver, LoginMarinaPage.class);
         companiesMarinaPage = PageFactory.initElements(driver,CompaniesMarinaPage.class);
@@ -31,19 +36,23 @@ public class LoginMarinaTest extends TestNgTestBase {
         driver.get(URL_LINK);
         loginMarinaPage.waitForLoginPageIsLoadedTime50();
 }
+    @BeforeClass(alwaysRun = true)
+    public void initWebDriver() {
+        driver = WebDriverPool.DEFAULT.getDriver(gridHubUrl, capabilities);
+    }
 
-    @Test
-    public void negativeLoginTest() {
-
-        loginMarinaPage.fillLoginField("sss")
-                        .fillPassField("pppp")
+    @Test(groups = {"smoke", "regression"},dataProviderClass = DataProviders.class, dataProvider = "invalidLogin")
+    public void negativeLoginTest(String login, String pass) {
+        Log.info("----Test: negativeLoginTest");
+        loginMarinaPage.fillLoginField(login)
+                        .fillPassField(pass)
                         .clickOnLoginButton()
                         .waitForLoginPageIsLoaded();
         Assert.assertTrue(loginMarinaPage.isOnLoginPage(),"Not passed. User is not on login page");
     }
 
-    @Test
-    public void negativeLoginTestPswIncorrect() {
+    //@Test(groups = "regression")
+    /*public void negativeLoginTestPswIncorrect() {
 
         loginMarinaPage.fillLoginField(LOGIN_ADM)
                         .fillPassField("hjkwi")
@@ -51,11 +60,12 @@ public class LoginMarinaTest extends TestNgTestBase {
         //companiesMarinaPage.waitForCompanyLoaded("Tadiran"); - it doesn't work (with dynamic element)
         companiesMarinaPage.waitForTadiranProjectLoaded();
         Assert.assertTrue(loginMarinaPage.isOnLoginPage(), "Not passed. User is not on login page");
-    }
+    } */
 
 
-    @Test
+    @Test(groups = {"smoke", "regression"})
     public void positiveAdminLoginTest() throws InterruptedException {
+        Log.info("----Test: positiveLoginTest");
         loginMarinaPage.fillLoginField(LOGIN_ADM)
                         .fillPassField(PSW_ADM)
                         .clickOnLoginButton();
