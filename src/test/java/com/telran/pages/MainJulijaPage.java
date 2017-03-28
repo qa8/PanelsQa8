89e1f5de2de622e8fe1ffa11006bdbdbefb0a1a9
julija1971 <kuzmina100@gmail.com>
2017-03-28 15:27:15 +0200

    Added new class

---
+++ src/test/java/com/telran/pages/MainJulijaPage.java
@@ -0,0 +1,41 @@
+package com.telran.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.PageFactory;
+
+
+/**
+ * Sample page
+ */
+public class MainJulijaPage extends Page {
+
+    @FindBy(how = How.TAG_NAME, using = "h1")
+    public WebElement header;
+
+    @FindBy(id = "login")
+    WebElement loginField;
+
+    @FindBy(id = "pass")
+    WebElement passwordField;
+
+    @FindBy(id = "loginButton")
+    WebElement loginButton;
+
+    public MainJulijaPage(WebDriver driver) {
+        super(driver);
+        PageFactory.initElements(driver, this);
+    }
+
+    //methods
+    public void fillLoginField(String text) {
+        setElementText(loginField, text);
+    }
+
+    public void clickOnLoginButton() {
+        clickElement(loginButton);
+
+    }
+}
